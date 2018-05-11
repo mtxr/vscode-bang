6bb12b4ced41ab4b5f01184f4814ffa0d5a26b7f
Matheus Teixeira <matheus.mtxr@gmail.com>
2018-05-10 23:30:33 -0300

    Running with selections

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 # Bang!
 
-Call command palette, select `Bang! Run command`, type your shell statment and BANG!
-
 > Under development...
+
+## History
+
+One day a friend of mine expert in Vim told me that VSCode would not be able to run shell commands like Vim.
+
+Sorry friend, yes, we can!
+
+## Usage
+
+Call command palette, select `Bang! Run command and insert`, type your shell statment and BANG!
+
+You can also run commands that doesn't insert any text with `Bang! Run command`.
+
+You can refer to the current opened file as `%`. Eg. `cat % | grep 'text' > newfile`.
+
+If you have selections, each selected range will be piped (not realy piped, but..) to you command.
+
+Eg. `echo "Selected text\nOther line from selection" | sort -r`
--- src/extension.ts
+++ src/extension.ts
@@ -8,8 +8,12 @@ interface ShellOutput {
     stderr: DescriptorOutput;
 }
 
+const currentFileRegEx = /([^\\])%/g;
+let lastCommand: string | undefined = undefined;
+
 const exec = (cmd: string): Promise<ShellOutput> => new Promise((resolve, reject) => {
-    execOriginal(cmd, (error, stdout, stderr) => {
+    const cwd = workspace.workspaceFolders && workspace.workspaceFolders.length > 0 ? workspace.workspaceFolders[0].uri.fsPath : undefined;
+    execOriginal(cmd, { cwd }, (error, stdout, stderr) => {
         if (error) { return reject(error); }
         return resolve({ stdout: stdout && stdout.toString(), stderr: stderr && stderr.toString() });
     });
@@ -30,31 +34,48 @@ async function getOrCreateEditor(forceCreate = false): Promise<TextEditor> {
     }
     return window.activeTextEditor as TextEditor;
 }
+function parseCommand(command: string) {
+    if (!window.activeTextEditor) {
+        return [command];
+    }
+    const editor = window.activeTextEditor;
+    command = command.replace(currentFileRegEx, `$1${editor.document.uri.fsPath}`);
+    if (editor.selections.length > 0) {
+        return editor.selections
+           .map((selection) => editor.document.getText(selection).length > 0
+                ? `echo "${(editor as TextEditor).document.getText(selection)}" | ${command}`
+                : command
+            );
+    }
+    return [command];
+}
 
-async function insertText(text: string, forceCreate = false) {
-    const editor = await getOrCreateEditor(forceCreate);
-    editor.edit((edit) => {
-        editor.selections.forEach((cursor) => edit.insert(cursor.active, text));
+async function run (insert = false) {
+    const command = await window.showInputBox({
+        value: lastCommand,
+        prompt: 'Shell command',
+        placeHolder: '! ls %',
+    });
+    if (!command) { return; }
+
+    lastCommand = command;
+
+    parseCommand(command).forEach(async (cmd, index) => {
+        try {
+            const { stdout } = await exec(cmd);
+            if (!stdout) { return; }
+            if (!insert || !stdout) { return; }
+            const editor = await getOrCreateEditor();
+            editor.edit((edit) => {
+                edit.replace((editor as TextEditor).selections[index], stdout);
+            });
+        } catch(e) {
+            window.showErrorMessage(e.message ? (e as Error).message : e.toString());
+        }
     });
 }
 
-function parseCommand(command: string) {
-    if (window.activeTextEditor) {
-        return command.replace(/([^\\])%/g, `$1${window.activeTextEditor.document.uri.fsPath}`);
-    }
-    return command;
-}
-
 export function activate(context: ExtensionContext) {
-
-    context.subscriptions.push(commands.registerCommand('bang.runCommand', async() => {
-        const command = await window.showInputBox({
-            prompt: 'Shell command',
-            placeHolder: '! ls %',
-        });
-        if (!command) { return; }
-        const { stdout } = await exec(parseCommand(command));
-        if (!stdout) { return; }
-        insertText(stdout);
-    }));
+    context.subscriptions.push(commands.registerCommand('bang.runCommand', () => run()));
+    context.subscriptions.push(commands.registerCommand('bang.runCommandAndInsert', () => run(true)));
 }
